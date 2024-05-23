7a07951c43212ded52a4b25304f5912dc2e5f372
Şenol Tanrıver <senoltanriver@gmail.com>
2024-05-23 12:00:48 +0300

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      <h1>Anne Sayfa</h1>
       <nav>
         <ul>
           <li><Link to="/testinga">Testinga</Link></li>
